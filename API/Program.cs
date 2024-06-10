a467cdb200c966ec0a8b118bd245e2a857c31cb6
Aaron Bunker <abunker@crankyape.com>
2024-06-10 15:22:15 -0500

    test

--- API/Program.cs
+++ API/Program.cs
@@ -10,12 +10,10 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
 app.MapControllers();
 
