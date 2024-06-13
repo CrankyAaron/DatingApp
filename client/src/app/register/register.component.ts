5f6fa58e2e7114aa3d89e2f60fa175458992d8a1
Aaron Bunker <abunker@crankyape.com>
2024-06-13 09:32:00 -0500

    Pre register

---
+++ client/src/app/register/register.component.ts
@@ -0,0 +1,21 @@
+import { Component, EventEmitter, Input, Output, output } from '@angular/core';
+
+@Component({
+    selector: 'app-register',
+    templateUrl: './register.component.html',
+    styleUrl: './register.component.css'
+})
+export class RegisterComponent {
+    @Input() usersFromHomeComponent: any;
+    @Output() cancelRegister = new EventEmitter();
+    model: any = {}
+
+    register() {
+        console.log(this.model);
+    }
+
+    cancel() {
+        this.cancelRegister.emit(false);
+    }
+
+}
