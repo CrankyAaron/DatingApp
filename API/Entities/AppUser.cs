da5045fbc43b50e6ec8bb8fe095006eebeeb720c
Aaron Bunker <abunker@crankyape.com>
2024-06-14 08:46:42 -0500

    Section 8 partial

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using API.Extensions;
 
 namespace API.Entities
 {
@@ -7,15 +8,23 @@ namespace API.Entities
         public int Id { get; set; }
         [Required]
         public string UserName { get; set; }
-        // public string Gender { get; set; }
-        // public string Introudction { get; set; }
+        public string Gender { get; set; }
+        public string Introduction { get; set; }
+        public string LookingFor { get; set; }
+        public string Interests { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
-        // public DateOnly DateOfBirth { get; set; }
-        // public string KnownAs { get; set; }
-        // public DateTime Created { get; set; } = DateTime.UtcNow;
-        // public DateTime LastActive { get; set; } = DateTime.UtcNow;
+        public DateTime DateOfBirth { get; set; }
+        public string KnownAs { get; set; }
+        public DateTime Created { get; set; } = DateTime.UtcNow;
+        public DateTime LastActive { get; set; } = DateTime.UtcNow;
 
-
+        public List<Photo> Photos { get; set; } = new List<Photo>();
+        public int GetAge()
+        {
+            return DateOfBirth.CalculateAge();
+        }
     }
 }
